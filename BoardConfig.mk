e73ed82378b27df363385ea240c48c5b4afaa56c
baizhi958216 <1475289190@qq.com>
2021-05-01 13:17:26 +0800

    m1971: Remove Twrp app

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,6 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXCLUDE_TWRPAPP := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
